742441c6a13a5dc7ed165d1995ada20744cfd467
Taylor Short <treddshort@gmail.com>
2018-05-14 12:58:28 -0700

    add Album router and component but no link

---
+++ src/components/Album.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import '../App.css';
+
+
+class Album extends Component {
+  render() {
+    return (
+      <div className="App">
+        <main>
+            <h1>Album component</h1>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default Album;

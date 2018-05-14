99d70b554a6c56e985740d612f6f608ae2a83d2c
Taylor Short <treddshort@gmail.com>
2018-05-14 15:25:19 -0700

    add content to landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import '../App.css';
 
+const Landing = () => (
+  <section className="library">
+    <h1 className="hero-title">Turn the music up!</h1>
 
-class Landing extends Component {
-  render() {
-    return (
-      <div className="App">
-        <main>
-            <h1>Hello!</h1>
-        </main>
+    <section className="selling-points">
+      <div className="point">
+        <h2 className="point-title">Choose your music</h2>
+        <p className="point-description">The world is full of music; why should you have to listen to music that someone else will choose.</p>
       </div>
-    );
-  }
-}
+      <div className="point">
+        <h2 className="point-title">Unlimited streaming, ad-free</h2>
+        <p className="point-description">No arbitrary limits. No distractions.</p>
+      </div>
+      <div className="point">
+        <h2 className="point-title">Mobile enabled</h2>
+        <p className="point-description">Listen to your music on the go. This streaming service is available on all mobile platforms.</p>
+      </div>
+    </section>
+  </section>
+);
+
 
 export default Landing;
--- src/components/Library.js
+++ src/components/Library.js
@@ -2,17 +2,11 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 import '../App.css';
 
+const Library = () => (
+  <section className="library">
+    Library will go here
+  </section>
+);
 
-class Library extends Component {
-  render() {
-    return (
-      <div className="App">
-        <main>
-            <h1>Hi</h1>
-        </main>
-      </div>
-    );
-  }
-}
 
 export default Library;

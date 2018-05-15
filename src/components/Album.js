aca24e9355972ac709287954cd25b788143d20b7
Taylor Short <treddshort@gmail.com>
2018-05-15 08:52:29 -0700

    add state to album.js and add table

--- src/components/Album.js
+++ src/components/Album.js
@@ -19,7 +19,7 @@ class Album extends Component {
     return (
       <section className="album">
         <section id="album-info">
-           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title}/>
+           <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
            <div className="album-details">
              <h1 id="album-title">{this.state.album.title}</h1>
              <h2 className="artist">{this.state.album.artist}</h2>

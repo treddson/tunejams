7f17446026a6b18197d047f1df8ef95652876510
Taylor Short <treddshort@gmail.com>
2018-05-17 07:54:36 -0700

    add playback ability and toggle between play and pause

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,9 +12,39 @@ class Album extends Component {
         return album.slug === this.props.match.params.slug
       });
       this.state = {
-          album: album
+          album: album,
+          currentSong: album.songs[0],
+          isPlaying: false
         };
+
+        this.audioElement = document.createElement('audio');
+        this.audioElement.src = album.songs[0].audioSrc;
       }
+
+      play() {
+        this.audioElement.play();
+        this.setState({ isPlaying: true });
+      }
+      pause() {
+        this.audioElement.pause();
+        this.setState({ isPlaying: false });
+      }
+
+      setSong(song) {
+        this.audioElement.src = song.audioSrc;
+        this.setState({ currentSong: song });
+      }
+
+      handleSongClick(song) {
+        const isSameSong = this.state.currentSong === song;
+        if (this.state.isPlaying && isSameSong) {
+          this.pause();
+        } else {
+          if (!isSameSong) { this.setSong(song); }
+          this.play();
+        }
+      }
+
   render() {
     return (
       <section className="album">
@@ -33,11 +63,11 @@ class Album extends Component {
             <col id="song-duration-column" />
           </colgroup>
           <tbody>
-              {this.state.album.songs.map( (val, index) =>
-              <tr key={index}>
+              {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                 <td className="song-number"> {index+1}</td>
-                <td className="song-title"> {val.title}</td>
-                <td className="song-duration"> {val.duration}</td>
+                <td className="song-title"> {song.title}</td>
+                <td className="song-duration"> {song.duration}</td>
               </tr>
               )}
           </tbody>

596c2b5986922003a4b048b2522f4166fbade880
Taylor Short <treddshort@gmail.com>
2018-05-21 21:24:06 -0700

    fixed issue where ionicons weren't displaying in player bar

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ class Album extends Component {
       this.state = {
           album: album,
           currentSong: album.songs[0],
+          currentTime: 0,
+          duration: album.songs[0].duration,
           isPlaying: false
         };
 
@@ -108,6 +110,8 @@ class Album extends Component {
         </table>
         <PlayerBar isPlaying={this.state.isPlaying} 
         currentSong={this.state.currentSong} 
+        currentTime={this.audioElement.currentTime}
+        duration={this.audioElement.duration}
         handleSongClick={() => this.handleSongClick(this.state.currentSong)} 
         handlePrevClick={() => this.handlePrevClick()}
         handleNextClick={() => this.handleNextClick()}
--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -7,19 +7,26 @@ class PlayerBar extends Component {
             <section className="player-bar">
             <section id="buttons">
            <button id="previous" onClick={this.props.handlePrevClick}>
-             <span className="ion-skip-backward"></span>
+             <span className="ion-md-skip-backward"></span>
            </button>
            <button id="play-pause" onClick={this.props.handleSongClick} >
-             <span className={this.props.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+             <span className={this.props.isPlaying ? 'ion-md-pause' : 'ion-md-play'}></span>
            </button>
            <button id="next" onClick={this.props.handleNextClick}>
-             <span className="ion-skip-forward"></span>
+             <span className="ion-md-skip-forward"></span>
            </button>
          </section>
          <section id="time-control">                                                                    
-           <div className="current-time">–:––</div>
-           <input type="range" className="seek-bar" value="0" />
-           <div className="total-time">–:––</div>
+           <div className="current-time">{this.props.currentTime}</div>
+           <input 
+           type="range" 
+           className="seek-bar" 
+           value={(this.props.currentTime / this.props.duration) || 0}
+           max="1"
+           min="0"
+           step="0.01"
+           />
+           <div className="total-time">{this.props.duration}</div>
          </section>
          <section id="volume-control">
            <div className="icon ion-volume-low"></div>
